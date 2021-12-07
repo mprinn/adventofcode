89db49d2eb764877c70950f2571ab25bf1b301a8
Malcolm Prinn <malcolm.prinn@intel.com>
2021-12-07 08:53:36 +0000

    add day07

---
+++ day07/main.py
@@ -0,0 +1,40 @@
+import time
+import re
+import math
+import sys
+from collections import Counter
+
+
+def fuelCount(base, pos, crabs=1):
+    minC = min(base, pos)
+    maxC = max(base, pos) + 1
+    fc = sum([i - minC for i in range(minC, maxC)])
+    fc *= crabs
+    return fc
+
+
+def doIt(filename: str) -> None:
+    with open(filename) as f:
+
+        allCrabs = Counter(map(int, re.findall(r'\d+', f.readline())))
+        minF = 0
+        minLvl = 5
+        maxLvl = max(allCrabs)
+        for c in allCrabs:
+            minF += fuelCount(minLvl, c, allCrabs[c])
+        for l in range(maxLvl):
+            fCost = 0
+            for c in allCrabs:
+                fCost += fuelCount(l, c, allCrabs[c])
+                if fCost > minF:
+                    break
+            if fCost < minF:
+                minF = fCost
+        print(minF)
+
+
+startTime = time.time()
+doIt("day07/training.txt")
+doIt("day07/input.txt")
+endTime = time.time()
+print(f"Process finished --- {endTime - startTime} seconds ---")
